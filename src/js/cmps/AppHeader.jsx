a3ab36ea5242397d9e4b08c8267d6cb8b4c59eb8
roi yotvat <roiyot261098@gmail.com>
2022-01-18 23:57:12 +0200

    landing page update

--- src/js/cmps/AppHeader.jsx
+++ src/js/cmps/AppHeader.jsx
@@ -2,16 +2,18 @@ import { Link } from 'react-router-dom'
 
 export function AppHeader() {
     return (
-        <header className="main-header flex justify-between">
-            <h2 className="logo">Brix</h2>
-            <nav className="main-nav flex align-center">
-                <div>
-                    <Link to="/"> Collection</Link>
-                    <Link to="/editor"> Editor</Link>
-                    <Link to="/"> Templates</Link>
-                    <Link to="/"> Login</Link>
-                </div>
-            </nav>
+        <header className="main-header">
+            <div className="header-content flex justify-between">
+                <h2 className="logo">Brix</h2>
+                <nav className="main-nav flex align-center">
+                    <div className="container">
+                        <Link to="/"> Collection</Link>
+                        <Link to="/editor"> Editor</Link>
+                        <Link to="/"> Templates</Link>
+                        <Link to="/"> Login</Link>
+                    </div>
+                </nav>
+            </div>
         </header>
     )
 } 
--- src/js/pages/home-page/HomePage.jsx
+++ src/js/pages/home-page/HomePage.jsx
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom'
 
 export function HomePage() {
     return (
         <section className="home">
-            <section>
+            <section className="top-fold flex column align-center">
+                <p>Your Dreams are  <span>Always</span>  possible, this time with ease.</p>
                 <div>
                     <img className="hero" src="https://media.istockphoto.com/photos/three-multiethnic-construction-workers-chatting-picture-id1332558192?s=612x612" alt="hero" />
                 </div>
-                <button>Get Started!</button>
+                <Link to="/editor">Get Started!</Link>
+            </section>
+            <section className="below-fold flex justify-center">
+                <img className="editor-img" src="https://media.istockphoto.com/photos/pelican-swoop-picture-id493835012?b=1&k=20&m=493835012&s=170667a&w=0&h=5W9__AUKKBzag8rNQPtPnfjCuS-CqEYwW9lclQkU0ww=" alt="editor-img" />
             </section>
         </section>
     )
