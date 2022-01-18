53bd26f820b9d0ae55b18a5360b14d922cf9ae8d
roi yotvat <roiyot261098@gmail.com>
2022-01-18 22:12:30 +0200

    header

--- src/js/cmps/AppHeader.jsx
+++ src/js/cmps/AppHeader.jsx
@@ -2,9 +2,9 @@
 
 export function AppHeader() {
     return (
-        <header className="main-header">
+        <header className="main-header flex justify-between">
             <h2>LOGO</h2>
-            <ul>
+            <ul className="main-nav">
                 <li>Collection</li>
                 <li>Templates</li>
                 <li>Login</li>
