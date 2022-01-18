9e3af8ed903c17aec149f634644506541952bed3
roi yotvat <roiyot261098@gmail.com>
2022-01-18 22:22:01 +0200

    app header work

--- src/js/cmps/AppHeader.jsx
+++ src/js/cmps/AppHeader.jsx
@@ -1,14 +1,17 @@
-
+import { Link } from 'react-router-dom'
 
 export function AppHeader() {
     return (
         <header className="main-header flex justify-between">
             <h2>LOGO</h2>
-            <ul className="main-nav">
-                <li>Collection</li>
-                <li>Templates</li>
-                <li>Login</li>
-            </ul>
+            <nav className="main-nav flex align-center">
+                <div>
+                    <Link to="/"> Collection</Link>
+                    <Link to="/editor"> Editor</Link>
+                    <Link to="/"> Templates</Link>
+                    <Link to="/"> Login</Link>
+                </div>
+            </nav>
         </header>
     )
-}
+} 
