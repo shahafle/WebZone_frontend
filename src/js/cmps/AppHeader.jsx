1d13942e3735a89578aa86150ba1208da74d21d5
roi yotvat <roiyot261098@gmail.com>
2022-01-18 22:08:34 +0200

    started filling up

--- src/js/cmps/AppHeader.jsx
+++ src/js/cmps/AppHeader.jsx
@@ -4,6 +4,11 @@ export function AppHeader() {
     return (
         <header className="main-header">
             <h2>LOGO</h2>
+            <ul>
+                <li>Collection</li>
+                <li>Templates</li>
+                <li>Login</li>
+            </ul>
         </header>
     )
 }
