ab8178d27eda636466b7fbf70e745564efc945c4
shahaf <shahaf1131@gmail.com>
2022-01-20 00:10:04 +0200

    dynamic cmp edit fix

--- src/js/pages/editor-page/cmps/dynamic-cmp/DynamicCmp.jsx
+++ src/js/pages/editor-page/cmps/dynamic-cmp/DynamicCmp.jsx
@@ -1,18 +1,19 @@
-// import { wapElService } from '../../../../services/wap-el.service'
+import { wapElService } from '../../../../services/wap-el.service'
 
 export function DynamicCmp(props) {
    const { cmp, onSetCurrElement } = props
+   const style = wapElService.getScaleUnits(cmp.style)
    switch (cmp.type) {
       case 'txt':
-         return <p style={cmp.style} onClick={(ev) => onSetCurrElement(ev, cmp)} >{cmp.txt}</p>
+         return <p style={style} onClick={(ev) => onSetCurrElement(ev, cmp)} >{cmp.txt}</p>
       case 'img':
          return <div>
-            <img style={cmp.style} src={cmp.url} onClick={(ev) => onSetCurrElement(ev, cmp)} alt="No img" />
+            <img style={style} src={cmp.url} onClick={(ev) => onSetCurrElement(ev, cmp)} alt="No img" />
          </div>
       case 'btn':
-         return <a style={cmp.style} onClick={(ev) => onSetCurrElement(ev, cmp)} href='##'>{cmp.txt}</a>
+         return <a style={style} onClick={(ev) => onSetCurrElement(ev, cmp)} href='##'>{cmp.txt}</a>
       case 'section':
-         return <section onClick={(ev) => onSetCurrElement(ev, cmp)} style={cmp.style} >
+         return <section onClick={(ev) => onSetCurrElement(ev, cmp)} style={style} >
             {cmp.cmps && cmp.cmps.map(c => <DynamicCmp key={c.id} cmp={c} onSetCurrElement={onSetCurrElement} />)}
          </ section>
 
